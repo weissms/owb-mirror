6a2470b2837851538e848569de31a240d4d950f1
jchaffraix <jchaffraix@a3cd4a6d-042f-0410-9b26-d8d12826d3fb>
2009-01-08 15:43:24 +0000

    2009-01-08  Julien Chaffraix  <jchaffraix@pleyo.com>
            Reviewed by Mario Bensi. 
    
            - Split the APITests into a one for WebView (WebViewTests.cpp) and one for WebFrame (WebFrameTests.cpp).
    
            - Corrected some coding style issues.
    
            * OWBLauncher/CMakeLists.txt:
            * OWBLauncher/Tests/APITests.cpp: Removed.
            * OWBLauncher/Tests/WebFrameTests.cpp: Copied from WebKitTools/OWBLauncher/Tests/APITests.cpp.
            (WebFrameTest::setUp):
            (WebFrameTest::tearDown):
            (WebFrameTest::testInitialValue): Test the WebFrame initial values.
            (main):
            * OWBLauncher/Tests/WebViewTests.cpp: Copied from WebKitTools/OWBLauncher/Tests/APITests.cpp.
            (WebViewTest::setUp):
            (WebViewTest::tearDown):
            (WebViewTest::testInitialValue): Test the WebView initial values.
            (WebViewTest::testInitWithFrame): Test initWithFrame method.
            (main):
    
    
    
    
    git-svn-id: http://www.sand-labs.org/svn@774 a3cd4a6d-042f-0410-9b26-d8d12826d3fb

---
+++ trunk/WebKitTools/OWBLauncher/Tests/WebViewTests.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+#include <cppunit/TestRunner.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+
+#include <WebKit.h>
+
+class WebViewTest : public CPPUNIT_NS::TestCase
+{
+    CPPUNIT_TEST_SUITE(WebViewTest);
+    CPPUNIT_TEST(testInitialValue);
+    CPPUNIT_TEST(testInitWithFrame);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    void setUp(void) {}
+    void tearDown(void) {} 
+
+protected:
+    void testInitialValue()
+    {
+        WebView* view = WebView::createInstance();
+        CPPUNIT_ASSERT(view);
+        CPPUNIT_ASSERT(!view->mainFrame());
+    }
+
+    void testInitWithFrame()
+    {
+        WebView* view = WebView::createInstance();
+        BalRectangle dummyRectangle;
+        const char* foobarGroup = "foobarGroup";
+        const char* foobarFrame = "foobarFrame";
+        view->initWithFrame(dummyRectangle, foobarFrame, foobarGroup);
+        CPPUNIT_ASSERT(!strncmp(foobarGroup, view->groupName(), strlen(foobarGroup)));
+        CPPUNIT_ASSERT(!strncmp(foobarFrame, view->mainFrame()->name(), strlen(foobarFrame)));
+    }
+
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(WebViewTest);
+
+int main( int ac, char **av )
+{
+    //--- Create the event manager and test controller
+    CPPUNIT_NS::TestResult controller;
+
+    //--- Add a listener that colllects test result
+    CPPUNIT_NS::TestResultCollector result;
+    controller.addListener( &result );        
+
+    //--- Add a listener that print dots as test run.
+    CPPUNIT_NS::BriefTestProgressListener progress;
+    controller.addListener( &progress );      
+
+    //--- Add the top suite to the test runner
+    CPPUNIT_NS::TestRunner runner;
+    runner.addTest( CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest() );
+    runner.run( controller );
+
+    return result.wasSuccessful() ? 0 : 1;
+}
