2f3849332465a55637d4f85b20150d2dadc7c9bf
odole <odole@a3cd4a6d-042f-0410-9b26-d8d12826d3fb>
2009-02-12 16:15:04 +0000

    2009-02-12  Olivier DOLE  <odole@pleyo.com>
            Remove unused gnome-vfs support from cmake.
    
            * cmake/CheckPlatformVideoSupport.cmake:
    
    
    
    git-svn-id: http://www.sand-labs.org/svn@842 a3cd4a6d-042f-0410-9b26-d8d12826d3fb

--- trunk/cmake/CheckPlatformVideoSupport.cmake
+++ trunk/cmake/CheckPlatformVideoSupport.cmake
@@ -1,16 +1,10 @@
 if(ENABLE_VIDEO)
     if(USE_GRAPHICS_GTK)
         pkg_check_modules(GSTREAMER_VIDEO REQUIRED gstreamer-video-0.10>=0.10.17)
-        pkg_check_modules(GNOMEVFS gnome-vfs-2.0>=2.0)
-        list(APPEND VIDEO_INCLUDE_DIRS
-            ${GSTREAMER_VIDEO_INCLUDE_DIRS}
-            ${GNOMEVFS_INCLUDE_DIRS}
-        )
-        list(APPEND VIDEO_LIBRARIES
-            ${GSTREAMER_VIDEO_LIBRARIES}
-            ${GNOMEVFS_LIBRARIES}
-        )
+        list(APPEND VIDEO_INCLUDE_DIRS ${GSTREAMER_VIDEO_INCLUDE_DIRS})
+        list(APPEND VIDEO_LIBRARIES ${GSTREAMER_VIDEO_LIBRARIES})
     endif(USE_GRAPHICS_GTK)
+
     if(USE_GRAPHICS_QT)
         pkg_check_modules(PHONON REQUIRED phonon)
         list(APPEND VIDEO_INCLUDE_DIRS ${PHONON_INCLUDE_DIRS})
