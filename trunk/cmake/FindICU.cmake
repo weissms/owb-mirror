83a5932fc1f215ca0cf460269271f16a4a7948b3
odole <odole@a3cd4a6d-042f-0410-9b26-d8d12826d3fb>
2010-01-04 14:07:30 +0000

    2010-01-04  Olivier DOLE  <odole@pleyo.com>
            Reviewed by Mario Bensi.
    
            Fix ICU check in cmake which was setting a problem with cross compîled
            builds.
    
            * cmake/FindICU.cmake:
    
    
    
    git-svn-id: http://www.sand-labs.org/svn@1241 a3cd4a6d-042f-0410-9b26-d8d12826d3fb

--- trunk/cmake/FindICU.cmake
+++ trunk/cmake/FindICU.cmake
@@ -1,23 +1,35 @@
-FIND_PROGRAM (ICU_CONFIG_EXECUTABLE icu-config)
-
-IF (NOT ICU_CONFIG_EXECUTABLE)
-    SET (ICU_FOUND FALSE)
-ELSE (NOT ICU_CONFIG_EXECUTABLE)
-
-EXEC_PROGRAM ("${ICU_CONFIG_EXECUTABLE} --ldflags-libsonly"
-     OUTPUT_VARIABLE ICU_LIBRARY
-     RETURN_VALUE ERROR_CODE
-)
-SEPARATE_ARGUMENTS(ICU_LIBRARY)
-
-EXEC_PROGRAM ("${ICU_CONFIG_EXECUTABLE} --cppflags-searchpath|sed s/^-I//"
-     OUTPUT_VARIABLE ICU_INCLUDE
-     RETURN_VALUE ERROR_CODE
-)
-SEPARATE_ARGUMENTS(ICU_INCLUDE)
-
-IF (ICU_LIBRARY)
-    SET (ICU_FOUND TRUE)
-ENDIF (ICU_LIBRARY)
-
-ENDIF (NOT ICU_CONFIG_EXECUTABLE)
+if(UNIX)
+    find_path(ICU_INCLUDE unicode/ucnv.h
+        /usr/include
+        /usr/local/include
+        /opt/local/include
+        /sw/include
+    )
+    find_library(ICU_I18N_LIBRARY
+        NAME icui18n
+        PATHS /usr/lib
+            /usr/local/lib
+            /opt/local/lib
+            /sw/lib
+    )
+    find_library(ICU_UC_LIBRARY
+        NAME icuuc
+        PATHS /usr/lib
+            /usr/local/lib
+            /opt/local/lib
+            /sw/lib
+    )
+    find_library(ICU_DATA_LIBRARY
+        NAME icudata
+        PATHS /usr/lib
+            /usr/local/lib
+            /opt/local/lib
+            /sw/lib
+    )
+    set(ICU_LIBRARY ${ICU_I18N_LIBRARY} ${ICU_UC_LIBRARY} ${ICU_DATA_LIBRARY})
+    if(ICU_INCLUDE)
+        set(ICU_FOUND TRUE)
+    else(ICU_INCLUDE)
+        set(ICU_FOUND FALSE)
+    endif(ICU_INCLUDE)
+endif(UNIX)
