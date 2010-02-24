4c9137c09ce8b2ad8f9219486a1cc9ce6d270c22
odole <odole@a3cd4a6d-042f-0410-9b26-d8d12826d3fb>
2010-02-24 13:55:29 +0000

    2010-02-24  Olivier DOLE  <odole@sand-labs.org>
            Just generate .tar.gz archive when running "make package"
    
            * cmake/Package.cmake:
    
    
    
    git-svn-id: http://www.sand-labs.org/svn@1386 a3cd4a6d-042f-0410-9b26-d8d12826d3fb

--- trunk/cmake/Package.cmake
+++ trunk/cmake/Package.cmake
@@ -1,9 +1,10 @@
 execute_process(COMMAND /usr/bin/lsb_release -sc
                 OUTPUT_VARIABLE CODENAME
                 ERROR_VARIABLE ERROR)
+set(CPACK_GENERATOR "TGZ")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Webkit OWB library is designed for embedded systems")
-set(CPACK_PACKAGE_CONTACT "Olivier DOLE <odole@pleyo.com>")
-set(CPACK_PACKAGE_VENDOR "Pleyo")
+set(CPACK_PACKAGE_CONTACT "Olivier DOLE <odole@sand-labs.org>")
+set(CPACK_PACKAGE_VENDOR "sand-labs")
 set(CPACK_PACKAGE_NAME "libwebkit-owb")
 set(CPACK_PACKAGE_VERSION 2.0.1)
 set(CPACK_PACKAGE_VERSION_MAJOR 2)
