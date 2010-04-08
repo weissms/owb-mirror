0d61955a3f1196ff18d24f799af951abced57618
gpenin <gpenin@a3cd4a6d-042f-0410-9b26-d8d12826d3fb>
2010-04-08 11:54:29 +0000

    Build fix after last webkit merge.
    git-svn-id: http://www.sand-labs.org/svn@1429 a3cd4a6d-042f-0410-9b26-d8d12826d3fb

--- trunk/BAL/Fonts/WebCore/SDLCairo/BCFontSDLCairo.cpp
+++ trunk/BAL/Fonts/WebCore/SDLCairo/BCFontSDLCairo.cpp
@@ -46,7 +46,7 @@ void Font::drawComplexText(GraphicsContext* context, const TextRun& run, const F
 {
 }
 
-float Font::floatWidthForComplexText(const TextRun& run, HashSet<const SimpleFontData*>* fallbackFonts) const
+float Font::floatWidthForComplexText(const TextRun& run, HashSet<const SimpleFontData*>* fallbackFonts, GlyphOverflow* /* glyphOverflow */) const
 {
     return 0.0;
 }
